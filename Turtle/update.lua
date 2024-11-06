71b2cb02ea7dc95654a9ddda423bfcfbf4518ef3
duniyash <80444721+duniyash@users.noreply.github.com>
2024-11-06 21:25:58 +0530

    test

--- Turtle/update.lua
+++ Turtle/update.lua
@@ -1,45 +1,55 @@
 local FILENAME = '/test.lua'
 
+local github = 'https://raw.githubusercontent.com/duniyash/MCBiomePred/refs/heads/main/Turtle/test.lua'
+
 local cacheBreak = tostring(math.random(0,0000))
 
-res, err = http.get('' .. cacheBreak)
-if not res then error(err) end
+res = http.get(github .. cacheBreak)
+if (res == nil) then 
 
-print("code retrieved")
+    print("code not retrieved")
+    print("error: " .. res.getResponseCode())
+    res.close()
 
-local newCode = res.readAll()
-
-if not (fs.exists(FILENAME)) then
-    local newHarvest = fs.open(FILENAME, 'w')
-    newHarvest.close()
-    print("harvest file created")
-end
-
-local readFile = fs.open(FILENAME, 'r')
-local oldCode = readFile.readAll()
-
-print("old file opened")
-
-readFile.close()
-
-local file = fs.open(FILENAME, 'w')
-
-if oldCode == newCode
-then
-    file.write(oldCode)
-    print('NO CHANGES MADE - Same Code')
 else
-    file.write(newCode)
-    print('WRITING UPDATE')
-    byteDiff = string.len(newCode) - string.len(oldCode)
- 
-    if byteDiff >= 0
-    then
-        print(tostring(math.abs(byteDiff)) .. ' bytes added')
-    else
-        print(tostring(math.abs(byteDiff)) .. ' bytes removed')
+
+    print("code retrieved")
+
+    local newCode = res.readAll()
+    
+    if not (fs.exists(FILENAME)) then
+        local newHarvest = fs.open(FILENAME, 'w')
+        newHarvest.close()
+        print("harvest file created")
     end
+    
+    local readFile = fs.open(FILENAME, 'r')
+    local oldCode = readFile.readAll()
+    
+    print("old file opened")
+    
+    readFile.close()
+    
+    local file = fs.open(FILENAME, 'w')
+    
+    if oldCode == newCode
+    then
+        file.write(oldCode)
+        print('NO CHANGES MADE - Same Code')
+    else
+        file.write(newCode)
+        print('WRITING UPDATE')
+        byteDiff = string.len(newCode) - string.len(oldCode)
+     
+        if byteDiff >= 0
+        then
+            print(tostring(math.abs(byteDiff)) .. ' bytes added')
+        else
+            print(tostring(math.abs(byteDiff)) .. ' bytes removed')
+        end
+    end
+     
+    file.close()
+    res.close()
+    
 end
- 
-file.close()
-res.close()
