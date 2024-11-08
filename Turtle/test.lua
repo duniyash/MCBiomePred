2c75c750212c01123c7d850a7f0d07d9b134fafd
Sachindu Jayasinghe <80444721+duniyash@users.noreply.github.com>
2024-11-08 11:53:31 +0530

    update test

--- Turtle/test.lua
+++ Turtle/test.lua
@@ -1,1 +1,15 @@
-print ("this is a test")
+print("this is a test")
+
+local function getEnv()
+	local env = {}
+	env[1] = turtle.inspectUp()
+	return env
+end
+
+local completeEnv = {}
+
+for i = 1, 2 do
+	completeEnv[i] = getEnv()
+	turtle.forward()
+end
+
